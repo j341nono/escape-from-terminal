58416a8a1be2b9c37197e1f08a09e1b8ee9c1b5f
j341nono <kanakana.nono1224@gmail.com>
2026-09-17 03:05:05 +0900

    feat: add radius collision with wall sliding

--- src/player.rs
+++ src/player.rs
@@ -1,6 +1,6 @@
 use std::f32::consts::TAU;
 
-use crate::{geom::Vec2, input::MovementInput};
+use crate::{geom::Vec2, input::MovementInput, map::Map};
 
 #[derive(Debug, Clone)]
 pub struct Player {
@@ -27,6 +27,18 @@ impl Player {
 
     pub fn movement_direction(&self, input: MovementInput) -> Vec2 {
         (self.forward() * input.forward + self.right() * input.strafe).normalized()
+    }
+
+    pub fn move_with_collision(&mut self, map: &Map, displacement: Vec2, radius: f32) {
+        let next_x = Vec2::new(self.position.x + displacement.x, self.position.y);
+        if map.is_walkable_position(next_x, radius) {
+            self.position.x = next_x.x;
+        }
+
+        let next_y = Vec2::new(self.position.x, self.position.y + displacement.y);
+        if map.is_walkable_position(next_y, radius) {
+            self.position.y = next_y.y;
+        }
     }
 }
 
@@ -57,5 +69,16 @@ mod tests {
         let mut player = Player::new(Vec2::new(1.0, 1.0), 0.0);
         player.rotate(-1.0, 2.0, 1.0);
         assert!(player.angle > 0.0 && player.angle < TAU);
+    }
+
+
+    #[test]
+    fn collision_prevents_wall_entry_and_allows_sliding() {
+        let map = Map::from_ascii(&["#####", "#.#.#", "#...#", "#...#", "#####"])
+            .expect("valid map");
+        let mut player = Player::new(Vec2::new(1.5, 1.5), 0.0);
+        player.move_with_collision(&map, Vec2::new(0.8, 0.6), 0.2);
+        assert_eq!(player.position.x, 1.5);
+        assert!(player.position.y > 1.5);
     }
 }
